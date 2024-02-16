3632a4ba1507918036c19e5327cc2519d0f39232
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-15 20:59:53 -0500

    sync

--- src/app/(api)/api/(auth)/users/password/route.ts
+++ src/app/(api)/api/(auth)/users/password/route.ts
@@ -1,5 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { renderPasswordResetEmail } from "@/app/components/emails/reset-password";
+import { sendEmail } from "@/app/lib/api/email/send-email";
+
 export function POST(req: NextRequest) {
+    const { text: textBody, html: htmlBody } = renderPasswordResetEmail({
+        userName: "munozarturo",
+        resetUrl: "example.com",
+        websiteUrl: "example.com",
+    });
+
+    sendEmail({
+        sender: "munoz.arturoroman@gmail.com",
+        recipient: "hsh47@case.edu",
+        subject: "Reset your password",
+        textBody: textBody,
+        htmlBody: htmlBody,
+    });
+
     return NextResponse.json({});
 }
