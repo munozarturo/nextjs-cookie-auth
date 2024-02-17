0ac3964f2709f3ac90dac2599ee07236388aa61a
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-17 15:59:43 -0500

    build(db): added error handling to db client connection

--- src/app/lib/db/client.ts
+++ src/app/lib/db/client.ts
@@ -1,23 +1,28 @@
 import { Database } from "./db.types";
 import { SupabaseClient as DatabaseClient } from "@supabase/supabase-js";
 import { createBrowserClient } from "@supabase/ssr";
+import { DatabaseConnectionError } from "../api/errors";
 
 function createClient(): DatabaseClient {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    if (!supabaseUrl) {
-        throw new Error(
-            "Environment variable `NEXT_PUBLIC_SUPABASE_URL` undefined."
-        );
-    }
+    try {
+        const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+        if (!supabaseUrl) {
+            throw new Error(
+                "Environment variable `NEXT_PUBLIC_SUPABASE_URL` undefined."
+            );
+        }
 
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-    if (!supabaseKey) {
-        throw new Error(
-            "Environment variable `NEXT_PUBLIC_SUPABASE_ANON_KEY` undefined."
-        );
-    }
+        const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+        if (!supabaseKey) {
+            throw new Error(
+                "Environment variable `NEXT_PUBLIC_SUPABASE_ANON_KEY` undefined."
+            );
+        }
 
-    return createBrowserClient<Database>(supabaseUrl, supabaseKey);
+        return createBrowserClient<Database>(supabaseUrl, supabaseKey);
+    } catch (e: any) {
+        throw new DatabaseConnectionError("Error connecting to database.", 500);
+    }
 }
 
 export { createClient as createDbClient, type DatabaseClient };
