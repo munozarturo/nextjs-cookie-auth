95cd7e19c3f2b244ce611ab88611ae4f892218bc
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-20 19:26:03 -0500

    build(api): added API actions

---
+++ src/lib/api/actions.ts
@@ -0,0 +1,71 @@
+import {
+    credentialsSchema,
+    emailSchema,
+    passwordSchema,
+    usernameSchema,
+} from "../validations/auth";
+
+import axiosInstance from "./axios-instance";
+import { z } from "zod";
+
+async function signIn(credentials: z.infer<typeof credentialsSchema>) {
+    const res = await axiosInstance.post("/api/signin", { credentials });
+
+    return res;
+}
+
+async function signOut() {
+    const res = await axiosInstance.post("/api/signout");
+
+    return res;
+}
+
+async function signUp(
+    username: z.infer<typeof usernameSchema>,
+    credentials: z.infer<typeof credentialsSchema>
+) {
+    const res = await axiosInstance.post("/api/signup", {
+        username,
+        credentials,
+    });
+
+    return res;
+}
+
+async function requestPasswordReset(email: z.infer<typeof emailSchema>) {
+    const res = await axiosInstance.post("/api/users/password/reset", {
+        email,
+    });
+
+    return res;
+}
+
+async function resetPassword(
+    resetId: string,
+    token: string,
+    newPassword: z.infer<typeof passwordSchema>
+) {
+    const res = await axiosInstance.post(
+        `/api/users/password/reset/${resetId}`,
+        { token, newPassword }
+    );
+
+    return res;
+}
+
+async function requestEmailVerification(userId: string) {
+    const res = await axiosInstance.post("/api/users/verification", { userId });
+
+    return res;
+}
+
+async function verifyEmail(verificationId: string, token: string) {
+    const res = await axiosInstance.post(
+        `/api/users/verify/${verificationId}`,
+        { token }
+    );
+
+    return res;
+}
+
+export const API = {};
